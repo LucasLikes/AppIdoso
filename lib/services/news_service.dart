ca53f81d669f17c87df87621844d3e00e5fb484a
LucasLikes <90657712+LucasLikes@users.noreply.github.com>
2025-05-06 12:44:13 -0300

    Update news_service.dart

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class NewsService {
-  final String apiKey = 'a5329211618709d01f9eb116e6195ed4'; // Sua chave de API
-  final String baseUrl = 'https://gnews.io/api/v4/search';
+  final String apiKey = 'kay'; // Sua chave de API
+  final String baseUrl = 'url';
 
   Future<List<Map<String, dynamic>>> fetchNews() async {
     final url = Uri.parse('$baseUrl?q=example&lang=en&country=us&max=10&apikey=$apiKey');
